b0e9a4c1546d57982c748a8ecc1265fe1febc481
Bhadran ss <ssbhadran10@gmail.com>
2025-08-01 14:32:26 +0530

    Update files

--- front-end/src/lib/axios.js
+++ front-end/src/lib/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:5000/api" // Local dev
-      : "https://ecommerce-z0zf.onrender.com", // Production full backend URL
+      ? "http://localhost:5000/api"
+      : "https://ecommerce-z0zf.onrender.com/api",
   withCredentials: true,
 });
 
