55c3a31b91e26581d151a7366d66b718812eccd0
Bhadran ss <ssbhadran10@gmail.com>
2025-08-01 15:02:37 +0530

    Update files

--- back-end/utils/jwt.js
+++ back-end/utils/jwt.js
@@ -30,7 +30,7 @@ const SendTokenWithCookie = async (res, user, message) => {
   const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   };
 
